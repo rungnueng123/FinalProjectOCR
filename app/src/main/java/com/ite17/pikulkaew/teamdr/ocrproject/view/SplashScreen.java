c7277cff6775018702bc88e4fac68bb5fe04b09e
Poobest Jirathitiphuwadon <j.poobest@Poobests-MacBook-Pro.local>
2017-08-30 15:02:11 +0700

    splashscreen

---
+++ app/src/main/java/com/ite17/pikulkaew/teamdr/ocrproject/view/SplashScreen.java
@@ -0,0 +1,15 @@
+package com.ite17.pikulkaew.teamdr.ocrproject.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.ite17.pikulkaew.teamdr.ocrproject.R;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+    }
+}
