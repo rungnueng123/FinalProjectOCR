d9d9116008c80d648f0782acf7954d1b4c27bf0f
DESKTOP-IDUP0PR\RUNGNUENG <rungnueng19@hotmail.com>
2017-09-11 20:05:05 +0700

    add Hamburger

--- app/src/main/java/com/ite17/pikulkaew/teamdr/ocrproject/view/DashboardActivity.java
+++ app/src/main/java/com/ite17/pikulkaew/teamdr/ocrproject/view/DashboardActivity.java
@@ -1,8 +1,13 @@
 package com.ite17.pikulkaew.teamdr.ocrproject.view;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -19,6 +24,9 @@ public class DashboardActivity extends AppCompatActivity {
     ImageView title_search_dashboard;
     ImageView more_search_dashboard;
     EditText edit_search_dashboard;
+    DrawerLayout drawerLayout;
+    ActionBarDrawerToggle actionBarDrawerToggle;
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -30,9 +38,47 @@ public class DashboardActivity extends AppCompatActivity {
 
     private void initInstance() {
 
-        title_search_dashboard = (ImageView) findViewById(R.id.title_search_dashboard);
-        more_search_dashboard = (ImageView) findViewById(R.id.more_search_dashboard);
-        edit_search_dashboard = (EditText) findViewById(R.id.edit_search_dashboard);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
 
+        if (toolbar != null){
+            setSupportActionBar(toolbar);
+        }
+
+        //title_search_dashboard = (ImageView) findViewById(R.id.title_search_dashboard);
+        //more_search_dashboard = (ImageView) findViewById(R.id.more_search_dashboard);
+        //edit_search_dashboard = (EditText) findViewById(R.id.edit_search_dashboard);
+
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+
+        actionBarDrawerToggle = new ActionBarDrawerToggle(
+                DashboardActivity.this,
+                drawerLayout,
+                R.string.open_drawer,
+                R.string.close_drawer
+        );
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+    }
+
+    @Override
+    protected void onPostCreate(@Nullable Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        actionBarDrawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        actionBarDrawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (actionBarDrawerToggle.onOptionsItemSelected(item))
+            return true;
+        return super.onOptionsItemSelected(item);
     }
 }
